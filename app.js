c113aa8d26b564820346343968e32b44d84e5c57
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-01-16 10:41:39 +0530

    Adding node functions

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (request, response) {
-  response.send('Hello World!');
+  response.write('Hello World endpoint');
+  response.end();
 });
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000');
+  console.log('Listening at localhost:3000');
 });
