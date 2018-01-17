52aa493d704532c25daaca175f68d6360df7ab6b
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-01-17 15:38:15 +0530

    Adding ajax call to add block

--- public/client.js
+++ public/client.js
@@ -10,4 +10,20 @@ $(document).ready(function () {
     $('.block-list').append(list);
   }
 
+  $('form').on('submit', function (e) {
+    console.log('submit clicked=>');
+    e.preventDefault();
+    var form = $(this);
+    var blockData = form.serialize();
+    $.ajax({
+      type: 'POST',
+      url: '/blocks',
+      data: blockData,
+    }).done(function (blockName) {
+      console.log('blockName=>', blockName);
+      appendToList([blockName]);
+      form.trigger('reset');
+    });
+  });
+
 });
